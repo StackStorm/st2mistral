d7502fe4a4aacfad19bb87d0e607d4481d2b9cd8
Matt Oswalt <oswaltm@brocade.com>
2017-07-11 18:07:22 -0700

    Implmented redirect functions for all filters
    Signed-off-by: Matt Oswalt <oswaltm@brocade.com>

--- st2mistral/functions/stackstorm.py
+++ st2mistral/functions/stackstorm.py
@@ -18,17 +18,102 @@ from six.moves import http_client
 from oslo_config import cfg
 from oslo_log import log as logging
 
+from mistral import exceptions as exc
+from st2common.jinja.filters import crypto
+from st2common.jinja.filters import data
+from st2common.jinja.filters import regex
+from st2common.jinja.filters import complex_type
+from st2common.jinja.filters import time
+from st2common.jinja.filters import version
+from st2common.jinja.filters import json_escape
+from st2common.util.jinja import use_none
+from st2mistral.utils import http
+
 from st2mistral import config
 config.register_opts()
 
-from mistral import exceptions as exc
-from st2mistral.utils import http
 
 LOG = logging.getLogger(__name__)
 
 
-def testfunc_(context, key):
-    return "FOOBAR"
+def decrypt_kv_(*args, **kwargs):
+    return crypto.decrypt_kv(*args, **kwargs)
+
+
+def to_json_string_(*args, **kwargs):
+    return data.to_json_string(*args, **kwargs)
+
+
+def to_yaml_string_(*args, **kwargs):
+    return data.to_yaml_string(*args, **kwargs)
+
+
+def to_complex_(*args, **kwargs):
+    return complex_type.to_complex(*args, **kwargs)
+
+
+def regex_match_(*args, **kwargs):
+    return regex.regex_match(*args, **kwargs)
+
+
+def regex_replace_(*args, **kwargs):
+    return regex.regex_replace(*args, **kwargs)
+
+
+def regex_search_(*args, **kwargs):
+    return regex.regex_search(*args, **kwargs)
+
+
+def regex_substring_(*args, **kwargs):
+    return regex.regex_substring(*args, **kwargs)
+
+
+def to_human_time_from_seconds_(*args, **kwargs):
+    return time.to_human_time_from_seconds(*args, **kwargs)
+
+
+def version_compare_(*args, **kwargs):
+    return version.version_compare(*args, **kwargs)
+
+
+def version_more_than_(*args, **kwargs):
+    return version.version_more_than(*args, **kwargs)
+
+
+def version_less_than_(*args, **kwargs):
+    return version.version_less_than(*args, **kwargs)
+
+
+def version_equal_(*args, **kwargs):
+    return version.version_equal(*args, **kwargs)
+
+
+def version_match_(*args, **kwargs):
+    return version.version_match(*args, **kwargs)
+
+
+def version_bump_major_(*args, **kwargs):
+    return version.version_bump_major(*args, **kwargs)
+
+
+def version_bump_minor_(*args, **kwargs):
+    return version.version_bump_minor(*args, **kwargs)
+
+
+def version_bump_patch_(*args, **kwargs):
+    return version.version_bump_patch(*args, **kwargs)
+
+
+def version_strip_patch_(*args, **kwargs):
+    return version.version_strip_patch(*args, **kwargs)
+
+
+def use_none_(*args, **kwargs):
+    return json_escape.json_escape(*args, **kwargs)
+
+
+def json_escape_(*args, **kwargs):
+    return use_none(*args, **kwargs)
 
 
 def st2kv_(context, key):
