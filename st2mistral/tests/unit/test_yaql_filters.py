4894d441413297f36e634c08187d26ca6a28eb79
Matt Oswalt <oswaltm@brocade.com>
2017-07-27 10:02:45 -0700

    Added tests for data, json escape, and regex filters
    Signed-off-by: Matt Oswalt <oswaltm@brocade.com>

--- st2mistral/tests/unit/test_yaql_filters.py
+++ st2mistral/tests/unit/test_yaql_filters.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 import json
+import yaml
 
 from st2mistral.tests.unit import test_filter_base as base
 
@@ -31,6 +32,203 @@ class TestCaseYaqlComplex(base.YaqlFilterTestCase):
         )
         actual_obj = json.loads(result)
         self.assertDictEqual(obj, actual_obj)
+
+
+class TestCaseYaqlData(base.YaqlFilterTestCase):
+
+    def test_filter_to_json_string(self):
+        obj = {'a': 'b', 'c': {'d': 'e', 'f': 1, 'g': True}}
+        result = YAQL_ENGINE('to_json_string($.k1)').evaluate(
+            context=self.get_yaql_context({'k1': obj})
+        )
+        actual_obj = json.loads(result)
+        self.assertDictEqual(obj, actual_obj)
+
+
+    def test_filter_to_yaml_string(self):
+        obj = {'a': 'b', 'c': {'d': 'e', 'f': 1, 'g': True}}
+        result = YAQL_ENGINE('to_yaml_string($.k1)').evaluate(
+            context=self.get_yaql_context({'k1': obj})
+        )
+        actual_obj = yaml.load(result)
+        self.assertDictEqual(obj, actual_obj)
+
+
+class TestCaseYaqlJsonEscape(base.YaqlFilterTestCase):
+
+    def test_doublequotes(self):
+        actual = YAQL_ENGINE('json_escape($.k1)').evaluate(
+            context=self.get_yaql_context({'k1': 'foo """ bar'})
+        )
+        expected = 'foo \\"\\"\\" bar'
+        self.assertEqual(actual, expected)
+
+    def test_backslashes(self):
+        actual = YAQL_ENGINE('json_escape($.k1)').evaluate(
+            context=self.get_yaql_context({'k1': 'foo \ bar'})
+        )
+        expected = 'foo \\\\ bar'
+        self.assertEqual(actual, expected)
+
+    def test_backspace(self):
+        actual = YAQL_ENGINE('json_escape($.k1)').evaluate(
+            context=self.get_yaql_context({'k1': 'foo \b bar'})
+        )
+        expected = 'foo \\b bar'
+        self.assertEqual(actual, expected)
+
+    def test_formfeed(self):
+        actual = YAQL_ENGINE('json_escape($.k1)').evaluate(
+            context=self.get_yaql_context({'k1': 'foo \f bar'})
+        )
+        expected = 'foo \\f bar'
+        self.assertEqual(actual, expected)
+
+    def test_newline(self):
+        actual = YAQL_ENGINE('json_escape($.k1)').evaluate(
+            context=self.get_yaql_context({'k1': 'foo \n bar'})
+        )
+        expected = 'foo \\n bar'
+        self.assertEqual(actual, expected)
+
+    def test_carriagereturn(self):
+        actual = YAQL_ENGINE('json_escape($.k1)').evaluate(
+            context=self.get_yaql_context({'k1': 'foo \r bar'})
+        )
+        expected = 'foo \\r bar'
+        self.assertEqual(actual, expected)
+
+    def test_tab(self):
+        actual = YAQL_ENGINE('json_escape($.k1)').evaluate(
+            context=self.get_yaql_context({'k1': 'foo \t bar'})
+        )
+        expected = 'foo \\t bar'
+        self.assertEqual(actual, expected)
+
+
+class TestCaseYaqlRegex(base.YaqlFilterTestCase):
+
+    def test_filters_regex_match(self):
+        context = {
+            'k1': 'xyz',
+            'pattern': u'x'
+        }
+        result = YAQL_ENGINE('regex_match($.k1, $.pattern)').evaluate(
+            context=self.get_yaql_context(context)
+        )
+        self.assertTrue(result)
+
+        context = {
+            'k1': 'xyz',
+            'pattern': u'y'
+        }
+        result = YAQL_ENGINE('regex_match($.k1, $.pattern)').evaluate(
+            context=self.get_yaql_context(context)
+        )
+        self.assertFalse(result)
+
+        context = {
+            'k1': 'v0.10.1',
+            'pattern': u'^v(\\d+\\.)?(\\d+\\.)?(\\*|\\d+)$'
+        }
+        result = YAQL_ENGINE('regex_match($.k1, $.pattern)').evaluate(
+            context=self.get_yaql_context(context)
+        )
+        self.assertTrue(result)
+
+    def test_filters_regex_replace(self):
+        context = {
+            'k1': u'xyz',
+            'pattern': u'x',
+            'replacement': u'y'
+        }
+        actual = YAQL_ENGINE('regex_replace($.k1, $.pattern, $.replacement)').evaluate(
+            context=self.get_yaql_context(context)
+        )
+        self.assertEqual(actual, 'yyz')
+
+        context = {
+            'k1': u'blue socks and red shoes',
+            'pattern': u'(blue|white|red)',
+            'replacement': u'color'
+        }
+        actual = YAQL_ENGINE('regex_replace($.k1, $.pattern, $.replacement)').evaluate(
+            context=self.get_yaql_context(context)
+        )
+        self.assertEqual(actual, 'color socks and color shoes')
+
+    def test_filters_regex_search(self):
+        context = {
+            'k1': 'xyz',
+            'pattern': u'x'
+        }
+        result = YAQL_ENGINE('regex_search($.k1, $.pattern)').evaluate(
+            context=self.get_yaql_context(context)
+        )
+        self.assertTrue(result)
+
+        context = {
+            'k1': 'xyz',
+            'pattern': u'y'
+        }
+        result = YAQL_ENGINE('regex_search($.k1, $.pattern)').evaluate(
+            context=self.get_yaql_context(context)
+        )
+        self.assertTrue(result)
+
+        context = {
+            'k1': 'v0.10.1',
+            'pattern': u'^v(\\d+\\.)?(\\d+\\.)?(\\*|\\d+)$'
+        }
+        result = YAQL_ENGINE('regex_search($.k1, $.pattern)').evaluate(
+            context=self.get_yaql_context(context)
+        )
+        self.assertTrue(result)
+
+
+    def test_filters_regex_substring(self):
+
+        # Normal (match)
+        context = {
+            'input_str': 'My address is 123 Somewhere Ave. See you soon!',
+            'pattern': u'([0-9]{3} \w+ (?:Ave|St|Dr))'
+        }
+        result = YAQL_ENGINE('regex_substring($.input_str, $.pattern)').evaluate(
+            context=self.get_yaql_context(context)
+        )
+        self.assertEqual(result, '123 Somewhere Ave')
+
+        # Selecting second match explicitly
+        context = {
+            'input_str': 'Your address is 567 Elsewhere Dr. My address is 123 Somewhere Ave.',
+            'pattern': u'([0-9]{3} \w+ (?:Ave|St|Dr))'
+        }
+        result = YAQL_ENGINE('regex_substring($.input_str, $.pattern, 1)').evaluate(
+            context=self.get_yaql_context(context)
+        )
+        self.assertEqual(result, '123 Somewhere Ave')
+
+        # Selecting second match explicitly, but doesn't exist
+        context = {
+            'input_str': 'Your address is 567 Elsewhere Dr.',
+            'pattern': u'([0-9]{3} \w+ (?:Ave|St|Dr))'
+        }
+        with self.assertRaises(IndexError):
+            result = YAQL_ENGINE('regex_substring($.input_str, $.pattern, 1)').evaluate(
+                context=self.get_yaql_context(context)
+            )
+
+        # No match
+        context = {
+            'input_str': 'My address is 986 Somewhere Ave. See you soon!',
+            'pattern': u'([0-3]{3} \w+ (?:Ave|St|Dr))'
+        }
+
+        with self.assertRaises(IndexError):
+            result = YAQL_ENGINE('regex_substring($.input_str, $.pattern, 1)').evaluate(
+                context=self.get_yaql_context(context)
+            )
+
 
 class TestCaseYaqlTime(base.YaqlFilterTestCase):
 
